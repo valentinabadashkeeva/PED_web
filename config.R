96456294ff1e42fced54ee74b8d985c1bfc6e63e
xprem003 <zendoor@seznam.cz>
2017-11-30 08:57:02 +0100

    new config

--- config.R
+++ config.R
@@ -1,4 +1,12 @@
+#library(git2r)
+#getwd()
+#repo = repository("C:/Users/Zendoor/Documents/PED_web")
+#config(repo, user.name = "xprem003", user.email = "zendoor@seznam.cz")
+
+
 library(git2r)
-getwd()
-repo = repository("C:/Users/Zendoor/Documents/PED_web")
-config(repo, user.name = "xprem003", user.email = "zendoor@seznam.cz")
+#repo = repository('/home/hanel/PED/PED_web/')
+repo = repository('C:/Users/Zendoor/Documents/PED_web')
+if (!"upstream" %in% remotes(repo)) remote_add(repo, 'upstream', 'https://github.com/hanel/PED_web.git')
+fetch(repo, 'upstream')
+merge('master', 'upstream')
